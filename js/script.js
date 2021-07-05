fdf6d22770c45b6e5ad2c29b9dc9515a3786b059
Natalia Paulino <56160992+daddariob@users.noreply.github.com>
2021-07-05 12:28:57 -0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form');
+form.addEventListener('submit',handleSubmit);
+
+function handleSubmit(e){
+
+}
+
+function playAgain(){
+    document.getElementById('btnRestart')
+}
